2a45900424bf6b785878b02d3fcd34506abc8a1b
Aurora Sky <62398803+melissa-clothilde@users.noreply.github.com>
2020-10-10 19:26:07 -0700

    begins authentication edits

--- server.js
+++ server.js
@@ -1,44 +1,55 @@
 // Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
+const express = require('express');
+const session = require('express-session');
+const flash = require('connect-flash');
+
 // Requiring passport as we've configured it
-const passport = require("./config/passport");
+const passport = require('./config/passport');
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
-const db = require("./models");
+const db = require('./models');
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Connect flash
+app.use(flash());
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+// Set Handlebars.
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-require("./routes/track-api-routes.js")(app);
-require("./routes/artist-api-routes.js")(app);
-require("./routes/books-api-routes.js")(app);
-
+require('./routes/html-routes.js')(app);
+require('./routes/users.js')(app);
+require('./routes/track-api-routes.js')(app);
+require('./routes/artist-api-routes.js')(app);
+require('./routes/books-api-routes.js')(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
       PORT,
       PORT
     );
